4d65c88fde0c2b59e42be45ee724f68eff898f5d
Ankit Tiwari <akdenied@gmail.com>
2023-01-12 23:23:51 +0530

    custom css

--- index.php
+++ index.php
@@ -16,9 +16,7 @@
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" type="text/css" href="vendors/styles/core.css">
-    <link rel="stylesheet" type="text/css" href="vendors/styles/icon-font.min.css">
-    <link rel="stylesheet" type="text/css" href="vendors/styles/style.css">
+    <?php include('./components/styles.php') ?>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
